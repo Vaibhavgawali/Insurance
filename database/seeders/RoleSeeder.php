eeffbba70bb06c771246ec7ce0ad9949d2b31d9e
Vaibhav Gawali <vaibhav@123@gmail.com>
2024-01-02 15:32:04 +0530

    roleseeder one line removed  and siderbar link commented

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -72,7 +72,6 @@ class RoleSeeder extends Seeder
         $institute_permissions=[
             $put_requirement,
             $view_modules,
-            $view_self_requirement_list,,
             $view_self_requirement_list
         ];
 
